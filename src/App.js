dfdded9c20cb7174c3dc41332379cbbffbdd68bd
devhiko <aydin96tr@gmail.com>
2022-04-13 01:49:55 +0300

    ready for dev

---
+++ src/App.js
@@ -0,0 +1,16 @@
+const Test = () => {
+  return (
+    <div>Test Div</div>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My App</h1>
+      <Test />
+    </div>
+  );
+}
+
+export default App;
